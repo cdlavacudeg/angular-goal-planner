a348a9c0402bae231873b5256b7f1bab6c4ab788
cdlavacudeg <cristianlav10@gmail.com>
2023-11-05 17:06:53 -0500

    [Add] Events

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,16 +5,26 @@ import { CommonModule } from '@angular/common';
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule],
-  template: ` <h1 [class]="dynamycClass">{{ title }}</h1> `,
+  templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title = 'Goal Planner';
   dynamycClass = ['dynamycClass', 'highlight', 'bold', 'italic'];
+  isRed = false;
 
   constructor() {
     setTimeout(() => {
       this.title = 'Project is working';
     }, 1000);
+  }
+
+  login() {
+    alert('Welcome');
+    this.title = 'User loged';
+  }
+
+  change() {
+    this.isRed = !this.isRed;
   }
 }
